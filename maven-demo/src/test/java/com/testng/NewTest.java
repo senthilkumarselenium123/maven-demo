dd79aadb3f9fcd0e06f7f3dff799db8ab1c42334
SenthilKumarSelenium <pskumartesting@gmail.com>
2022-01-16 01:45:59 +0530

    Paramater added

--- maven-demo/src/test/java/com/testng/NewTest.java
+++ maven-demo/src/test/java/com/testng/NewTest.java
@@ -1,4 +1,5 @@
 package com.testng;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 
@@ -6,8 +7,17 @@ import org.testng.annotations.Test;
 import com.code.App;
 
 public class NewTest {
+	
+  @Parameters("Browser")	
   @Test
-  public void f() {
+  public void f(String lBrowser) {
+	  if(lBrowser.equals("CHROME"))
+	  {
 		App.sample3();
+	  }else
+	  {
+		  App.sample3();
+		  App.sample3();
+	  }
   }
 }
